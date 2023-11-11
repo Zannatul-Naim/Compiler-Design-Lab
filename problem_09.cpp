bb4c961a58273d1096417636dd5e53fa668b3eea
Zannatul Naim <zannatulnaim09@gmail.com>
2023-11-11 19:17:10 +0600

    Update problem_09.cpp

--- problem_09.cpp
+++ problem_09.cpp
@@ -10,37 +10,46 @@ int main() {
     set<string> v = {"read", "eat", "take", "run", "write"};
 
     string sentence;
-    getline(cin, sentence);
+    freopen("9.txt", "r", stdin);
+    
+    while(getline(cin, sentence)) {
 
-    vector<string> words;
+        vector<string> words;
 
-    string tmp = "";
-    for(int i = 0; sentence[i]; i++) {
-        if(sentence[i] == ' ') {
-            words.push_back(tmp);
-            tmp = "";
-        } else {
-            tmp += sentence[i];
+        string tmp = "";
+        for(int i = 0; sentence[i]; i++) {
+            if(sentence[i] == ' ') {
+                words.push_back(tmp);
+                tmp = "";
+            } else {
+                tmp += sentence[i];
+            }
+        } words.push_back(tmp);
+
+        int cnt = 0;
+
+        if(words.size() > 3 or words.size() < 2) {
+            cout << sentence << ": Invalid Input" << endl;
+            break;
         }
-    } words.push_back(tmp);
-
-    int cnt = 0;
-
-    for(string word : words) {
-        cnt++;
-        if(cnt == 1 and !pn.count(word) and !p.count(word)) {
-            cout << "Syntactically Invalid!" << endl;
-            return 0;
-        } else if(cnt == 2 and !v.count(word)) {
-            cout << "Syntactically Invalid!" << endl;
-            return 0;
-        } else if(cnt == 3 and !n.count(word)) {
-            cout << "Syntactically Invalid!" << endl;
-            return 0;
+        bool isValid = true;
+        for(string word : words) {
+            cnt++;
+            if(cnt == 1 and !pn.count(word) and !p.count(word)) {
+                isValid = false;
+                break;
+            } else if(cnt == 2 and !v.count(word)) {
+                isValid = false;
+                break;
+            } else if(cnt == 3 and !n.count(word)) {
+                isValid = false;
+            }
         }
+        
+        if(isValid) cout << sentence << ": Syntactically Valid" << endl;
+        else cout << sentence << ": Syntactically Invalid!" << endl;
+
     }
-
-    cout << "Syntactically Valid" << endl;
 
     return 0;
 

d639b0f5b712c297d1db4ef881761f74d457f957
Zannatul Naim <zannatulnaim09@gmail.com>
2024-02-06 01:57:36 +0600

    Update problem_11.cpp

--- problem_11.cpp
+++ problem_11.cpp
@@ -14,16 +14,11 @@ struct Node {
 };
 
 bool isOperator(char c) {
-    return (c == '+' || c == '-' || c == '*' || c == '/');
+    return (c == '+' or c == '-' or c == '*' or c == '/' or c == '^');
 }
 
 int precedence(char c) {
-    if(c == '^')
-        return 3;
-    else if(c == '*' || c == '/')
-        return 2;
-    else if(c == '+' || c == '-')
-        return 1;
+    if(isOperator(c)) return 1;
     else
         return -1;
 }
@@ -34,24 +29,20 @@ string infixToPostfix(string infix) {
     int l = infix.length();
     string postfix;
     for(int i = 0; i < l; i++) {
-        if((infix[i] >= 'a' && infix[i] <= 'z')||(infix[i] >= 'A' && infix[i] <= 'Z')) {
+        if(isdigit(infix[i])) return postfix;
+        if(infix[i] != 'a' and infix[i] != 'b' and infix[i] != 'c' and !isOperator(infix[i]) and infix[i] != '(' and infix[i] != ')') return postfix;
+        if(infix[i] == 'a' or infix[i] == 'b' or infix[i] == 'c') {
             postfix+= infix[i];
-        }
-        else if(infix[i] == '(') {
+        } else if(infix[i] == '(') {
             st.push('(');
-        }
-        else if(infix[i] == ')') {
+        } else if(infix[i] == ')') {
             while(st.top() != 'N' && st.top() != '(') {
                 char c = st.top();
                 st.pop();
                 postfix += c;
             }
-            if(st.top() == '(') {
-                char c = st.top();
-                st.pop();
-            }
-        }
-        else {
+            st.pop();
+        } else {
             while(st.top() != 'N' && precedence(infix[i]) <= precedence(st.top())) {
                 char c = st.top();
                 st.pop();
@@ -69,7 +60,7 @@ string infixToPostfix(string infix) {
 }
 
 bool isOperand(char ch) {
-    return (ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || (ch >= '0' && ch <= '9');
+    return (ch == 'a' or ch == 'b' or ch == 'c');
 }
 
 Node* parse(string sentence) {
@@ -80,7 +71,7 @@ Node* parse(string sentence) {
         if (isOperand(ch)) {
             Node* node = new Node(ch);
             stack.push(node);
-        } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+        } else if (isOperator(ch)) {
             Node* node = new Node(ch);
             node->right = stack.top();
             stack.pop();
@@ -108,9 +99,59 @@ int main() {
     string sentence = "a*b+c";
 
     sentence = infixToPostfix(sentence);
-    // getline(cin, sentence);
     Node* tree = parse(sentence);
     print(tree);
     cout << endl;
+    // cout << sentence << endl;
     return 0;
 }
+
+
+/*
+//Syntax tree
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector<string> tree(50, "                              ");
+
+void syntaxTree(string ex);
+bool isOp(char x);
+
+int main()
+{
+    string expression;
+    // Input format = A+B*C/D*G+F-T
+    cin >> expression;
+    syntaxTree(expression);
+    cout << "Syntax Tree:\n";
+    for (string x : tree)
+    {
+        cout << x << "\n";
+    }
+}
+
+void syntaxTree(string ex)
+{
+    int row = 0, col = 2;
+    for (int i = 0; ex[i]; i++)
+    {
+        if (isOp(ex[i]))
+        {
+            tree[row][col] = ex[i];
+            tree[row + 1][col - 1] = '/';
+            tree[row + 1][col + 1] = '\\';
+            tree[row + 2][col - 2] = ex[i - 1];
+            row += 2;
+            col += 2;
+        }
+    }
+    tree[row][col] = ex.back();
+}
+
+bool isOp(char x)
+{
+    return (x == '+' or x == '-' or x == '*' or x == '\\');
+}
+
+*/

ee6d2f2efcc4b7a2d0c88aeff91e99ac05ec97b1
Zannatul Naim <zannatulnaim09@gmail.com>
2024-02-05 19:41:48 +0600

    Update problem_04.cpp

--- problem_04.cpp
+++ problem_04.cpp
@@ -2,9 +2,8 @@
 using namespace std;
 
 bool isInt(string word) {
-
+    if(word.size() == 0) return false;
     if(word[0] == '0') return false;
-
     for(int i = 1; i < word.size(); i++) {
         if(!isdigit(word[i])) return false;
     }
@@ -13,42 +12,32 @@ bool isInt(string word) {
 
 bool isVariable(string word) {
 
-    if(!(tolower(word[0] >= 'i' and tolower(word[0]) <= 'n'))) return false;
-
+    if(tolower(word[0]) < 'i' or tolower(word[0]) > 'n') return false;
     for(int i = 1; word[i]; i++) {
-        if(tolower(word[i] >= 'a' and tolower(word[i]) <= 'z')) continue;
-        else if(word[i] >= '0' or word[i] <= '9') continue;
+        if(isalnum(word[i])) continue;
         else return false;
     }
     return true;
 }
 
-
 int main() {
 
-    freopen("input.txt", "r", stdin);
+    freopen("4.txt", "r", stdin);
     string word;
     
     while(getline(cin, word)) {
 
         if(isVariable(word)) {
             cout << word << " : Integer Variable." << endl;
-        } else if(word.size() <= 4) {
-            if(isInt(word)) {
-                cout << word << " : Short Integer." << endl;
+        } else if(isInt(word)) {
+            if(word.size() <= 4) {
+                cout << word << " : Short Integer" << endl;
             } else {
-                cout << word << " : Invalid input | not integer | Undefined" << endl;
-            }
-        } else if(word.size() > 4 and word.size() <= 20) {
-            if(isInt(word)) {
-                cout << word << " : Long Integer." << endl;
-            } else {
-                cout << word << " : Invalid input | not integer | Undefined" << endl;
+                cout << word << " : Long Integer" << endl;
             }
         } else {
-            cout << word << " : Invalid input | not integer | Undefined" << endl;
+            cout << word << " : Invalid input" << endl;
         }
     }
-
     return 0;
 }

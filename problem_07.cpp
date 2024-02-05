4604c2ceb9f6edbcfafcd0834d3815079a1405fb
Zannatul Naim <zannatulnaim09@gmail.com>
2024-02-05 20:03:33 +0600

    Update problem_07.cpp

--- problem_07.cpp
+++ problem_07.cpp
@@ -2,8 +2,7 @@
 
 using namespace std;
 
-bool is_keyword(string x)
-{
+bool is_keyword(string x) {
     vector<string> keywords = {
         "true", "false",
         "or", "and", "xor",
@@ -21,16 +20,19 @@ bool is_keyword(string x)
     return false;
 }
 
-bool is_operator(string x)
-{
+bool is_operator(string x) {
     vector<string> operators = {
-        "+",
-        "-",
-        "*", 
-        "/",
-        "%"
+        "+", "-", "*", "/", "%",                                           // Arithmetic Operators
+        "==", "!=", "<", ">", "<=", ">=",                                  // Relational Operators
+        "&&", "||", "!",                                                   // Logical Operators
+        "&", "|", "^", "~", "<<", ">>",                                    // Bitwise Operators
+        "=", "+=", "-=", "*=", "/=", "%=", "&=", "|=", "^=", "<<=", ">>=", // Assignment Operators
+        "++", "--",                                                        // Increment and Decrement Operators
+        "?", ":",                                                          // Conditional Operator
+        ",",                                                               // Comma Operator
+        ".", "->",                                                         // Member Access Operators
+        "::"                                                               // Scope Resolution Operator
     };
-
     for (int i = 0; i < operators.size(); i++) {
         if (operators[i].compare(x) == 0) {
             return true;
@@ -41,14 +43,13 @@ bool is_operator(string x)
 
 bool id_identifier(string x) {
     if (isdigit(x[0])) return false;
-    if (x[0] == '_' or (tolower(x[0]) >= 'a' and tolower(x[0]) <= 'z')) {
-        return true;
+    for(char ch : x) {
+        if(ch != '_' and !isalnum(ch)) return false;
     }
-    return false;
+    return true;
 }
 
 bool is_string_constant(string x) {
-    
     char ch = '"';
     if (x[0] == ch and x[x.size() - 1] == ch) {
         return true;
@@ -56,6 +57,11 @@ bool is_string_constant(string x) {
 }
 
 bool is_constant(string x) {
+    int cnt = 0;
+    for(char ch : x) {
+        if(ch == '.') cnt++;
+    }
+    if(cnt > 1) return false;
     for (int i = 0; x[i]; i++) {
         if (isdigit(x[i]) or x[i] == '.') {
             continue;
@@ -65,16 +71,16 @@ bool is_constant(string x) {
 }
 
 int main() {
-
+    freopen("7.txt", "r", stdin);    
     string word;
-    cin >> word;
-
-    if (is_keyword(word)) cout << word << " is aReserved Keyword\n";
-    else if (id_identifier(word)) cout << word << " is an Identifier" << endl;
-    else if (is_operator(word)) cout << word << " is an Operator" << endl;
-    else if(is_string_constant(word)) cout << word << " is a String Constant" << endl;
-    else if (is_constant(word)) cout << word << " is a Constant" << endl;
-    else cout << word << " is Invalid" << endl;
+    while(getline(cin, word)) {
+        if (is_keyword(word)) cout << word << " is a Reserved Keyword\n";
+        else if (id_identifier(word)) cout << word << " is an Identifier" << endl;
+        else if (is_operator(word)) cout << word << " is an Operator" << endl;
+        else if(is_string_constant(word)) cout << word << " is a String Constant" << endl;
+        else if (is_constant(word)) cout << word << " is a Constant" << endl;
+        else cout << word << " is Invalid" << endl;
+    }
 
     return 0;
 }
